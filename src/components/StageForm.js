45e9fe49b675c663c2fdbf56698c7963f754af70
Sean Edwards <stedwards87@gmail.com>
2018-04-08 19:45:58 -0400

    getValidationMessages doesnt exist

--- src/components/StageForm.js
+++ src/components/StageForm.js
@@ -68,7 +68,7 @@ class StageForm extends Component {
               type="text"
               value={this.props.getStore()[key] || ""}
               onChange={this.handleChange.bind(this)} />
-            {this.props.getValidationMessages(key).map(this.renderHelpText)}
+            //{this.props.getValidationMessages(key).map(this.renderHelpText)}
           </Form.Field>);
         })}
       </Form >
