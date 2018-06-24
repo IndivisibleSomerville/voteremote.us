c35d4f3ba90b80e580b71ac944621d9eb9b113bf
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2018-06-24 13:05:32 -0400

    Intergrate validation in to StageForm.js

--- src/components/StageForm.js
+++ src/components/StageForm.js
@@ -1,73 +1,93 @@
-
 import React, { Component } from 'react';
-import { Input, Form } from 'semantic-ui-react'
+import { Input, Form } from 'semantic-ui-react';
+
+import Joi from 'joi';
 
 class StageForm extends Component {
   constructor(props) {
     super(props);
 
-    this.validatorTypes = this.makeValidationObject(this.props.form, (key) => this.props.form[key].validation);
-    console.log(this.validatorTypes)
+    // Setup this.state
+    this.state = this.setupState(
+      props.getStore(),
+      Object.keys(props.form)
+    );
 
-    this.getValidatorData = this.getValidatorData.bind(this);
-    this.renderHelpText = this.renderHelpText.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.isValidated = this.isValidated.bind(this);
   }
 
+  setupState(store, keys) {
+    let newState = {};
+
+    // Create "error" keys for each form value, and set it to null
+    keys.forEach((key) => {
+      let errorName = key + '_error';
+
+      newState[key] = store[key];
+      newState[errorName] = null;
+    });
+
+    return newState;
+  }
+
   isValidated() {
-    return new Promise((resolve, reject) => {
-      this.props.validate((error) => {
-        if (error) {
-          reject();
-          return;
-        }
-        resolve();
-      });
+    let isDataValid = true;
+    let newState = {};
+    let newStore = {};
+
+    // Iterate over props.form fields and update field's error status
+    Object.keys(this.props.form).forEach((key) => {
+      let schema = this.props.form[key].validation;
+      let value = this.state[key];
+      let validation = Joi.validate(value, schema);
+      let errorName = key + '_error';
+
+      if (validation.error !== null) {
+        isDataValid = false;
+        // set error message for given field property
+        newState[errorName] = validation.error.details[0].message;
+      } else {
+        // set null to error message value for given field property
+        newState[errorName] = null;
+        newStore[key] = value;
+      }
     });
+    // Update state and local store
+    this.setState(newState);
+    if (isDataValid) {
+      this.props.updateStore(newStore);
+    }
+
+    return isDataValid;
   }
 
-  getValidatorData() {
-    return this.makeValidationObject(this.props.form, (key) => this.props.getStore()[key])
-  }
+  onChange(e) {
+    let newState = {};
 
-  makeValidationObject(key, valueCb) {
-    return Object.entries(this.props.form)
-      .map(([key, value]) => {
-        return [key, valueCb(key)];
-      })
-      .filter(([key, value]) => value !== null && value !== undefined)
-      .reduce((result, [key, value]) => {
-        result[key] = value;
-        return result
-      }, {});
-  }
-
-  handleChange(event) {
-    const target = event.target;
-
-    this.props.updateStore({
-      [target.name]: target.type === 'checkbox'
-        ? target.checked
-        : target.value
-    });
-  }
-
-  renderHelpText(message, id) {
-    return (<div className="val-err-tooltip" key={id}><span>{message}</span></div>);
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
   }
 
   render() {
     return (
       <Form>
         {Object.entries(this.props.form).map(([key, value]) => {
+          let errorName = key + "_error";
+          let errorCn = this.state[errorName] == null ? "" : "ui red message";
+
           return (<Form.Field key={key}>
             <label>{value.label}</label>
             <Input
+              ref={key}
               name={key}
               placeholder={value.placeholder}
               type="text"
-              value={this.props.getStore()[key] || ""}
-              onChange={this.handleChange.bind(this)} />
+              value={this.state[key]}
+              onChange={this.onChange}
+              onBlur={this.isValidated}
+            />
+          <div className={errorCn}><span>{this.state[errorName]}</span></div>
           </Form.Field>);
         })}
       </Form >
@@ -75,4 +95,4 @@ class StageForm extends Component {
   }
 }
 
-export default StageForm; //validation(strategy)(StageForm);
+export default StageForm;
