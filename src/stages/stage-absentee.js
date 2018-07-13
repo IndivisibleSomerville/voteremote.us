5f42cd96ebf51cccd9b0fb782d2fd7a1d05ad47c
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2018-07-13 17:12:14 -0400

    Improve Absentee Ballot url query values.

--- src/stages/stage-absentee.js
+++ src/stages/stage-absentee.js
@@ -1,14 +1,14 @@
 export default {
   url: 'https://absentee.vote.org',
   id: 'vote-absentee-iframe',
-  firstName: 'user_first_name',
-  lastName: 'user_last_name',
-  streetAddressLine1: 'user_given_street_address',
-  streetAddressLine2: 'user_unit_number',
-  city: 'user_city',
-  state: 'user_state',
-  zipCode: 'user_zip_5',
-  email: 'user_email',
-  phone: 'user_digits',
-  birthDate: ['user_date_of_birth_month', 'user_date_of_birth_day', 'user_date_of_birth_year']
+  firstName: 'user[first_name]',
+  lastName: 'user[last_name]',
+  streetAddressLine1: 'user[given_street_address]',
+  streetAddressLine2: 'user[unit_number]',
+  city: 'user[city]',
+  state: 'user[state]',
+  zipCode: 'user[zip_5]',
+  email: 'user[email]',
+  phone: 'user[digits]',
+  birthDate: ['user[date_of_birth_month]', 'user[date_of_birth_day]', 'user[date_of_birth_year]']
 };
