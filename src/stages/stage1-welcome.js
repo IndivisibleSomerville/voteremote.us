07e6824f153cf3b6023635f30f8f400149ecd49d
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2018-07-26 23:39:48 -0400

    Fix next functionality in stage1

--- src/stages/stage1-welcome.js
+++ src/stages/stage1-welcome.js
@@ -16,17 +16,5 @@ export default {
   phone: {
     validation: Joi.string().regex(/^([0-9]){3}(-)?([0-9]){3}(-)?([0-9]){4}$/),
     errorMsg: "Not a valid phone number! Should be in xxx-xxx-xxxx format!"
-  },
-  birthMonth: {
-    validation: Joi.string().regex(/^(0[1-9]|1[012])$/),
-    errorMsg: "Birth month must be in MM format!"
-  },
-  birthDay: {
-    validation: Joi.string().regex(/^(0[1-9]|[12][0-9]|3[01])$/),
-    errorMsg: "Birth day must be in DD format!"
-  },
-  birthYear: {
-    validation: Joi.string().regex(/^(19|20)\d\d$/),
-    errorMsg: "Birth year must be in 19YY or 20YY format!"
   }
 }
